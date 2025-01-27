793bd0750242e0d8394c27c1b13861b3ddf6f4eb
Luca <lukasjob@gmail.com>
2025-01-27 16:32:37 +0100

    lenear_regression

--- dlwp/linear_regression.py
+++ dlwp/linear_regression.py
@@ -4,7 +4,29 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 
+# Data Generation
+true_b = 1
+true_w = 2
+N = 100
+
+np.random.seed(42)
+x = np.random.rand(N, 1)
+epsilon = (.1 * np.random.randn(N, 1))
+y = true_b + true_w * x + epsilon
+
+idx = np.arange(N)
+np.random.shuffle(idx)
+
+train_idx = idx[:int(N*.8)]
+val_idx = idx[int(N*.8):]
+
+x_train, y_train = x[train_idx], y[train_idx]
+x_val, y_val = x[val_idx], y[val_idx]
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+x_train_tensor = torch.as_tensor(x_train).float().to(device)
+y_train_tensor = torch.as_tensor(y_train).float().to(device)
 
 lr=0.1
 
@@ -13,3 +35,15 @@ b = torch.randn(1, requires_grad=True, dtype=torch.float, device=device)
 w = torch.randn(1, requires_grad=True, dtype=torch.float, device=device)
 
 optimizer = optim.SGD([b, w], lr=lr)
+
+n_epochs = 1000
+
+for epoch in range(n_epochs):
+    y_hat = b + w * x_train_tensor
+    error = (y_hat - y_train_tensor)
+    loss = (error ** 2).mean()
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+print(b, w)
